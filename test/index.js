331137fd4ded05029e1f4ff5b17b4c9da63a4850
Federico Bond <federicobond@gmail.com>
2017-06-16 10:07:23 -0300

    Format tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
-var fs = require("fs");
-var SolidityParser = require("../src/index");
+var fs = require("fs")
+var SolidityParser = require("../src/index")
 
 describe("#parse", () => {
   it("parses test file correctly", () => {
-    var content = fs.readFileSync(__dirname + "/test.sol");
-    SolidityParser.parse(content.toString());
+    var content = fs.readFileSync(__dirname + "/test.sol")
+    SolidityParser.parse(content.toString())
   });
 });
