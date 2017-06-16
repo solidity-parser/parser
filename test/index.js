0e504b0674fc575b8f71d80e1c3daba7785a9e61
Federico Bond <federicobond@gmail.com>
2017-06-16 03:41:31 -0300

    Remove arrow functions

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 var fs = require("fs");
 var SolidityParser = require("../src/index");
 
-describe("#parse", () => {
-  it("parses test file correctly", () => {
+describe("#parse", function() {
+  it("parses test file correctly", function() {
     var content = fs.readFileSync(__dirname + "/test.sol");
     SolidityParser.parse(content.toString());
   });
