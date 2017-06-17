d5f411573d373d6d2ad37eb734458aee571da1dd
Federico Bond <federicobond@gmail.com>
2017-06-16 22:49:48 -0300

    Fix test case

--- src/ErrorListener.js
+++ src/ErrorListener.js
@@ -1,10 +1,11 @@
 var antlr4 = require("../antlr4/index")
 
 function ErrorListener() {
+  antlr4.error.ErrorListener.call(this)
   this._errors = []
 }
 
-ErrorListener.prototype = Object.create(antlr4.error.ErrorListener)
+ErrorListener.prototype = Object.create(antlr4.error.ErrorListener.prototype)
 ErrorListener.prototype.constructor = ErrorListener
 
 ErrorListener.prototype.syntaxError = function(
