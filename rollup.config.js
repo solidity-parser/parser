82ca23a063b76a208b09c66855f9b2c0f85867cc
Franco Victorio <victorio.franco@gmail.com>
2020-12-06 13:56:26 -0300

    Emit both node and browser modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,27 @@
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import pkg from './package.json';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+
 const extensions = [
   '.js', '.ts',
 ];
 
 export default {
   input: './src/index.ts',
-  output: {
-    dir: 'dist',
+  output: [{
+    file: pkg.main,
     format: 'cjs',
     sourcemap: true
-  },
+  }, {
+    file: pkg.browser,
+    format: 'iife',
+    name: 'SolidityParser',
+    sourcemap: true,
+  }],
   plugins: [
+    nodePolyfills(),
     resolve({ extensions }),
     commonjs(),
     babel({
