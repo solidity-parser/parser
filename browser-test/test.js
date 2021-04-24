5ce049f63f894adf32f9a9ce4b960b8da74caefb
Franco Victorio <victorio.franco@gmail.com>
2021-04-24 00:23:51 -0300

    Add browser smoke test with karma

---
+++ browser-test/test.js
@@ -0,0 +1,11 @@
+it('smoke test for browser', () => {
+  if (SolidityParser === undefined) {
+    throw new Error('SolidityParser is not available')
+  }
+
+  const ast = SolidityParser.parse('contract Foo {}')
+
+  if (ast.type !== 'SourceUnit') {
+    throw new Error('Parsing failed')
+  }
+})
