efc7abed91115dcca5db90ae748d110887f2b7f4
Franco Victorio <victorio.franco@gmail.com>
2021-04-27 14:11:47 -0300

    Add integration test

---
+++ test-integration/test.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const path = require('path')
+const { assert } = require('chai')
+
+const parser = require('..')
+
+describe('integration', function () {
+  it('parses test file correctly', function () {
+    const content = fs.readFileSync(
+      path.join(__dirname, '..', 'test', 'test.sol')
+    )
+    parser.parse(content.toString())
+  })
+
+  it('parses an empty string', function () {
+    const ast = parser.parse('')
+    assert.deepEqual(ast, {
+      type: 'SourceUnit',
+      children: [],
+    })
+  })
+
+  it('tokenizes correctly', function () {
+    const tokens = parser.tokenize('contract Foo {} // a comment')
+
+    assert.deepEqual(tokens, [
+      {
+        type: 'Keyword',
+        value: 'contract',
+      },
+      {
+        type: 'Identifier',
+        value: 'Foo',
+      },
+      {
+        type: 'Punctuator',
+        value: '{',
+      },
+      {
+        type: 'Punctuator',
+        value: '}',
+      },
+      {
+        type: 'Keyword',
+        value: '// a comment',
+      },
+    ])
+  })
+})
