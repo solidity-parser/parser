5fb78bf612ac30f2aabd88e1f00f8c48a1ea89ab
Klaus Hott <klahott@gmail.com>
2023-11-26 13:01:16 +1300

    building tokens-string instead of processing it on run time and using the filesystem

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,7 +1,7 @@
 import { Token, AntlrToken, TokenizeOptions } from './types'
-import untypedTokens from './tokens-string'
+import untypedTokens from './antlr/solidity-tokens'
 
-const tokens = untypedTokens as string
+const tokens = untypedTokens as { [key: string]: string }
 
 const TYPE_TOKENS = [
   'var',
@@ -14,21 +14,6 @@ const TYPE_TOKENS = [
   'Fixed',
   'UFixed',
 ]
-
-function rsplit(str: string, value: string) {
-  const index = str.lastIndexOf(value)
-  return [str.substring(0, index), str.substring(index + 1, str.length)]
-}
-
-function normalizeTokenType(value: string) {
-  if (value.endsWith("'")) {
-    value = value.substring(0, value.length - 1)
-  }
-  if (value.startsWith("'")) {
-    value = value.substring(1, value.length)
-  }
-  return value
-}
 
 function getTokenType(value: string) {
   if (value === 'Identifier' || value === 'from') {
@@ -56,24 +41,12 @@ function getTokenType(value: string) {
   }
 }
 
-function getTokenTypeMap() {
-  return tokens
-    .split('\n')
-    .map((line) => rsplit(line, '='))
-    .reduce((acum: { [key: number]: string }, [value, key]) => {
-      acum[parseInt(key, 10)] = normalizeTokenType(value)
-      return acum
-    }, {})
-}
-
 export function buildTokenList(
   tokensArg: AntlrToken[],
   options: TokenizeOptions
 ): Token[] {
-  const tokenTypes = getTokenTypeMap()
-
   const result = tokensArg.map((token) => {
-    const type = getTokenType(tokenTypes[token.type])
+    const type = getTokenType(tokens[token.type.toString()])
     const node: Token = { type, value: token.text }
     if (options.range === true) {
       node.range = [token.startIndex, token.stopIndex + 1]
