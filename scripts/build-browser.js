54116a83eb2eb06c55b482517451f54a718ae9a7
Klaus <klahott@gmail.com>
2023-12-21 13:01:23 -0300

    reversing for now the update on esbuild since there was a bug in the build on prettier-plugin-solidity

--- scripts/build-browser.js
+++ scripts/build-browser.js
@@ -1,12 +1,5 @@
 const path = require('path')
 const { build } = require('esbuild')
-const browserslist = require('browserslist')
-const { resolveToEsbuildTarget } = require('esbuild-plugin-browserslist')
-
-const target = resolveToEsbuildTarget(
-  browserslist(['>0.5%', 'not dead', 'not op_mini all']),
-  { printUnknownTargets: false }
-)
 
 build({
   logLevel: 'info',
@@ -14,7 +7,6 @@ build({
   bundle: true,
   sourcemap: true,
   format: 'iife',
-  target,
   globalName: 'SolidityParser',
   outfile: path.resolve(__dirname, '../dist/index.iife.js'),
 }).catch(() => process.exit(1))
